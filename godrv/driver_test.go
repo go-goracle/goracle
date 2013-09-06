19483e01020180a98132b4636017fed1683e0d31
Tamás Gulácsi <gt-waterhouse@gthomas.eu>
2013-09-06 22:38:47 +0200

    little bit better test for select CLOB

--- godrv/driver_test.go
+++ godrv/driver_test.go
@@ -19,8 +19,11 @@ package godrv
 import (
 	"database/sql"
 	"flag"
+	"strings"
 	"testing"
-	"time"
+	//"time"
+
+	"github.com/tgulacsi/goracle/oracle"
 )
 
 var fDsn = flag.String("dsn", "", "Oracle DSN")
@@ -30,31 +33,32 @@ func TestSimple(t *testing.T) {
 	defer conn.Close()
 
 	var (
-		err  error
-		num  int
-		nul  interface{}
-		rat  float64
-		raw  []byte
-		str  string
-		date time.Time
-		//dur time.Duration
-	)
-	for i, tst := range []struct {
-		qry string
+		err error
 		dst interface{}
-	}{
-		{"SELECT ROWNUM FROM DUAL", &num},
-		{"SELECT LOG(10, 2) FROM DUAL", &rat},
-		{"SELECT 'árvíztűrő tükörfúrógép' FROM DUAL", &str},
-		{"SELECT HEXTORAW('00') FROM DUAL", &raw},
-		{"SELECT TO_DATE('2006-05-04 15:07:08', 'YYYY-MM-DD HH24:MI:SS') FROM DUAL", &date},
-		{"SELECT NULL FROM DUAL", &nul},
+	)
+	for i, qry := range []string{
+		"SELECT ROWNUM FROM DUAL",
+		"SELECT LOG(10, 2) FROM DUAL",
+		"SELECT 'árvíztűrő tükörfúrógép' FROM DUAL",
+		"SELECT HEXTORAW('00') FROM DUAL",
+		"SELECT TO_DATE('2006-05-04 15:07:08', 'YYYY-MM-DD HH24:MI:SS') FROM DUAL",
+		"SELECT NULL FROM DUAL",
+		"SELECT TO_CLOB('árvíztűrő tükörfúrógép') FROM DUAL",
 	} {
-		row := conn.QueryRow(tst.qry)
-		if err = row.Scan(tst.dst); err != nil {
-			t.Errorf("%d. error with %q test: %s", i, tst.qry, err)
+		row := conn.QueryRow(qry)
+		if err = row.Scan(&dst); err != nil {
+			t.Errorf("%d. error with %q test: %s", i, qry, err)
 		}
-		t.Logf("%d. %q result: %#v", i, tst.qry, tst.dst)
+		t.Logf("%d. %q result: %#v", i, qry, dst)
+		if strings.Index(qry, " TO_CLOB(") >= 0 {
+			clob := dst.(*oracle.ExternalLobVar)
+            b, e := clob.ReadAll()
+            if e != nil {
+                t.Errorf("error reading clob (%v): %s", clob, e)
+            } else {
+			t.Logf("clob=%s", b)
+        }
+		}
 	}
 
 	qry := "SELECT rn, CHR(rn) FROM (SELECT ROWNUM rn FROM all_objects WHERE ROWNUM < 256)"
@@ -68,6 +72,10 @@ func TestSimple(t *testing.T) {
 		t.Errorf("error getting columns for %q: %s", qry, err)
 	}
 	t.Logf("columns for %q: %v", qry, cols)
+	var (
+		num int
+		str string
+	)
 	for rows.Next() {
 		if err = rows.Scan(&num, &str); err != nil {
 			t.Errorf("error scanning row: %s", err)
