419168d6dcac06033c4956900ac00ce8c0a2185a
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-03-25 22:22:34 +0100

    driver_test: better error report

--- godrv/driver_test.go
+++ godrv/driver_test.go
@@ -103,7 +103,7 @@ func TestPrepared(t *testing.T) {
 	defer conn.Close()
 	stmt, err := conn.Prepare("SELECT ? FROM DUAL")
 	if err != nil {
-		t.Errorf("error preparing query: %v", stmt)
+		t.Errorf("error preparing query: %v", err)
 		t.FailNow()
 	}
 	rows, err := stmt.Query("a")
