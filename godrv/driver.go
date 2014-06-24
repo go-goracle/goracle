e6972bb3d55a834ce5768fcae3550b6b4974c209
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-06-24 12:53:37 +0200

    remove extra log.Printf (thanks, @vvekic!)

--- godrv/driver.go
+++ godrv/driver.go
@@ -210,7 +210,6 @@ func (r rowsRes) Next(dest []driver.Value) error {
 	row := (*[]interface{})(unsafe.Pointer(&dest))
 	// log.Printf("FetcOneInto(%p %+v len=%d) %T", row, *row, len(*row), *row)
 	err := r.cu.FetchOneInto(*row...)
-	log.Printf("fetched row=%p %#v (len=%d) err=%v", row, *row, len(*row), err)
 	debug("fetched row=%p %#v (len=%d) err=%v", row, *row, len(*row), err)
 	if err != nil {
 		if err == io.EOF {
