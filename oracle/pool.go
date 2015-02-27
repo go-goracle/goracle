b0537a969a5d5f8795d6674c040ab72220fe42ff
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-02-27 18:51:53 +0100

    add pool

---
+++ oracle/pool.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2015 Tamás Gulácsi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package oracle
+
+import (
+	"sync"
+)
+
+// Pool is a simple Pool for connections.
+type Pool struct {
+	user, passw, sid string
+	pool             *sync.Pool
+}
+
+// NewPool returns a new pool.
+func NewPool(dsn string) Pool {
+	p := Pool{pool: &sync.Pool{}}
+	p.user, p.passw, p.sid = SplitDSN(dsn)
+	return p
+}
+
+// Get an old/new connection.
+func (p Pool) Get() (*Connection, error) {
+	cx := p.pool.Get()
+	if cx != nil {
+		return cx.(*Connection), nil
+	}
+	return NewConnection(p.user, p.passw, p.sid, false)
+}
+
+// Put the connection back to the pool.
+func (p Pool) Put(cx *Connection) {
+	if cx == nil {
+		return
+	}
+	_ = cx.Rollback()
+	p.pool.Put(cx)
+}
---
+++ oracle/pool_test.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2015 Tamás Gulácsi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package oracle
+
+import "testing"
+
+func TestPool(t *testing.T) {
+	p := NewPool(*dsn)
+	cx1, err := p.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("1. %p", cx1.handle)
+	defer cx1.Close()
+	cx2, err := p.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("2. %p", cx2.handle)
+	if cx1.handle == cx2.handle {
+		t.Errorf("got the same for second, wanted different")
+	}
+	defer cx2.Close()
+	p.Put(cx2)
+	cx3, err := p.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("3. %p", cx3.handle)
+	defer cx3.Close()
+	if cx3.handle != cx2.handle {
+		t.Errorf("put back %v, got %v", cx2, cx3)
+	}
+}
