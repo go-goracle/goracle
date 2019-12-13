72835e8a960dbc21a6c746f5d1f81f8c881ea58d
Tamás Gulácsi <tgulacsi78@gmail.com>
2019-12-13 08:24:19 +0100

    Goracle is deprecated because of naming (trademark) issues. Please use github.com/godror/godror instead!

--- deprecate.go
+++ deprecate.go
@@ -7,12 +7,3 @@ func init() {
 	fmt.Println("Goracle is deprecated because of naming (trademark) issues. Please use github.com/godror/godror instead!")
 	fmt.Fprintln(os.Stderr, "Goracle is deprecated because of naming (trademark) issues. Please use github.com/godror/godror instead!")
 }
-package goracle
-
-import "os"
-import "fmt"
-
-func init() {
-	fmt.Println("Goracle is deprecated because of naming (trademark) issues. Please use github.com/godror/godror instead!")
-	fmt.Fprintln(os.Stderr, "Goracle is deprecated because of naming (trademark) issues. Please use github.com/godror/godror instead!")
-}
